d1fe1a5c8a46a5f084b2ff211bd4ccd6c4710818
PRINCE LIVE <princesaxena3545@gmail.com>
2026-09-15 15:52:21 +0530

    Fix import

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 import os
 
-BOT_TOKEN = os.environ["8831139468:AAEIgFkpbXsYCZ_qF14xLZeDJaK4bLDee9c"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 ADMIN_ID = 7796466520
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
